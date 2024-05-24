c49dd2864dc6c991fad9df093744944776e85734
dontLoveby <142394137+dontLoveby@users.noreply.github.com>
2024-05-25 07:19:32 +0800

    添加细节

--- demo5main.js
+++ demo5main.js
@@ -2,16 +2,20 @@
 // revise 100
 // 游戏数据
 var gameData = {
-	width: 10,
-	height: 10,
-	blank: " ^",
-	door: "门",
-	wall1: "墙",
-	playerX: 3,
-	playerY: 3,
-	playerWorldX: 0,
-	playerWorldY: 0,
-	world : {},
+	window: {
+	    width: 30,
+	    height: 20,
+	    scale: 10,
+	    offsetX: 0,
+	    offsetY: 0,
+		blank: " ^",
+		door: "门",
+		wall: "墙",
+		playerX: 3,
+		playerY: 3,
+		playerWorldX: 0,
+		playerWorldY: 0
+	},
 	enemy : function(x = 4, y = 5) {
 		return {
 			text : "敌",
@@ -19,15 +23,37 @@ var gameData = {
 			Y : y
 		}
 	},
-	anyWall : function(x = 0, y = 0, wallstr = "墙") {
+	anyWall : function(x = 0, y = 0, wall_str = "墙") {
 		return {
-			text : wallstr,
+			text : wall_str,
 			X : x,
 			Y : y
 		}
 	},
 	enemies : [],
-	walls : []
+	walls : [],
+	world : {/*区块*/
+		size : 10,// 已加载的区块数量
+		hash : {/* 区块哈希表 */},
+		load : function(x, y) {// 加载区块
+		    var a = gameData.world.hash[x + "," + y];
+		    if (a === undefined) {// 区块未加载
+		        gameData.world.hash[x + "," + y] = a = gameData.world.size * gameData.world.size;
+		        for (var i = - gameData.world.size / 2; i < gameData.world.size / 2; i++) {
+					for (var j = - gameData.world.size / 2; j < gameData.world.size / 2; j++) {
+						var b = gameData.world.hash[i + "," + j];
+						if (b === undefined) {
+							gameData.world.hash[i + "," + j] = b = gameData.world.size * gameData.world.size;
+							gameData.walls.push(gameData.anyWall(i, j));
+							// gameData.walls.push(gameData.anyWall(i, j, "墙"));
+							// gameData.walls.push(gameData.anyWall(i, j, "敌"));	
+							// gameData.walls.push(gameData.anyWall(i, j, "门"));
+						}
+					}
+				}
+		    }
+		}
+	}
 };
 const duration = 1; // 倒计时时长，单位为秒
 const displayElement = document.getElementById("timer1"); // 用于显示计时器的元素
@@ -42,27 +68,32 @@ var gameBoard = document.getElementById('game-board');
 // 生成世界
 function generateWorld() {
 	var board = '';
-	for (var y = 0; y < gameData.height; y++) {
-		for (var x = 0; x < gameData.width; x++) {
-			var isPlayer = x === gameData.playerX && y === gameData.playerY;
+	for (var i = 0; i < gameData.window.height; i++) {
+		for (var x = 0; x < gameData.window.width; x++) {
+			var isPlayer = x === gameData.window.playerX && i === gameData.window.playerY;
 			var isEnemy = gameData.enemies.some(function(enemy) {
-				return enemy.X === x && enemy.Y === y;
+				return enemy.X === x && enemy.Y === i;
 			});
-			var isWall = gameData.walls.some(function(wall) {
-				return wall.X === x && wall.Y === y;
+			var isWall = gameData.window.walls.some(function(wall) {
+				return wall.X === x && wall.Y === i;
 			});
 		}
 	}
 	// 生成世界的逻辑
+	for (var i = - gameData.world.size / 2, a = 0; i < gameData.world.size / 2; i++) {
+		for (var j = - gameData.world.size / 2; j < gameData.world.size / 2; j++, a++) {
+			gameData.world.hash[i + "," + j] = a;
+		}
+	}
 }
 
 // 更新游戏界面
 function updateGameBoard() {
 	var board = '';
 	// document.getElementById("debug").textContent = "timer=" + timer.getValue();
-	for (var y = 0; y < gameData.height; y++) {
-		for (var x = 0; x < gameData.width; x++) {
-			var isPlayer = x === gameData.playerX && y === gameData.playerY;
+	for (var y = 0; y < gameData.window.height; y++) {
+		for (var x = 0; x < gameData.window.width; x++) {
+			var isPlayer = x === gameData.window.playerX && y === gameData.window.playerY;
 			var isEnemy = false;
 			var enemyText = "敌";
 
@@ -76,7 +107,7 @@ function updateGameBoard() {
 				} else if (isEnemy) {
 					board += enemyText;
 				} else {
-					board += gameData.blank;
+					board += gameData.window.blank;
 				}
 			}
 		}
@@ -84,20 +115,20 @@ function updateGameBoard() {
 	}
 	var value = timer.getValue();
 	if (value === 0) {
-		for (var i = 0; i < gameData.enemies.length; i++) {
+		for (var i = 0; i < gameData.enemies.length; i++) {// 敌人移动
 			do {
 				var randomInt = Math.floor(Math.random() * 5);// 随机0~4
 				var X = gameData.enemies[i].X;
 				var Y = gameData.enemies[i].Y;
-				if (randomInt === 0 && !isPositionOccupied(X + 1, Y) && !isLeaveedge(X + 1, Y))
+				if (randomInt === 0 && !isPositionOccupied(X + 1, Y) && !isLeaveEdge(X + 1, Y))
 					X += 1;
-				else if (randomInt === 1 && !isPositionOccupied(X - 1, Y) && !isLeaveedge(X - 1, Y))
+				else if (randomInt === 1 && !isPositionOccupied(X - 1, Y) && !isLeaveEdge(X - 1, Y))
 					X -= 1;
-				else if (randomInt === 2 && !isPositionOccupied(X, Y + 1) && !isLeaveedge(X, Y + 1))
+				else if (randomInt === 2 && !isPositionOccupied(X, Y + 1) && !isLeaveEdge(X, Y + 1))
 					Y += 1;
-				else if (randomInt === 3 && !isPositionOccupied(X, Y - 1) && !isLeaveedge(X, Y - 1))
+				else if (randomInt === 3 && !isPositionOccupied(X, Y - 1) && !isLeaveEdge(X, Y - 1))
 					Y -= 1;
-				}while (X === gameData.playerX || Y === gameData.playerY);
+			}while (X === gameData.window.playerX || Y === gameData.window.playerY);// 如果敌人移动到玩家位置，重新生成位置
 			gameData.enemies[i].X = X;
 			gameData.enemies[i].Y = Y;
 		}
@@ -113,44 +144,44 @@ document.addEventListener('keyup', function(event) {
 	switch (event.key) {
 		case ("a"):
 		case ("ArrowLeft"): { // 左箭头键
-			var newX = gameData.playerX - 1;
-			var newY = gameData.playerY;
+			var newX = gameData.window.playerX - 1;
+			var newY = gameData.window.playerY;
 
-			if (!isPositionOccupied(newX, newY) && !isLeaveedge(newX, newY)) {
-				gameData.playerX = newX;
+			if (!isPositionOccupied(newX, newY) && !isLeaveEdge(newX, newY)) {
+				gameData.window.playerX = newX;
 				gameData.playerWorldX -= 1;
 			}
 			break;
 		}
 		case ("w"):
 		case ("ArrowUp"): { // 上箭头键
-			var newX = gameData.playerX;
-			var newY = gameData.playerY - 1;
+			var newX = gameData.window.playerX;
+			var newY = gameData.window.playerY - 1;
 
-			if (!isPositionOccupied(newX, newY) && !isLeaveedge(newX, newY)) {
-				gameData.playerY = newY;
+			if (!isPositionOccupied(newX, newY) && !isLeaveEdge(newX, newY)) {
+				gameData.window.playerY = newY;
 				gameData.playerWorldY -= 1;
 			}
 			break;
 		}
 		case ("d"):
 		case("ArrowRight"): { // 右箭头键
-			var newX = gameData.playerX + 1;
-			var newY = gameData.playerY;
+			var newX = gameData.window.playerX + 1;
+			var newY = gameData.window.playerY;
 
-			if (!isPositionOccupied(newX, newY) && !isLeaveedge(newX, newY)) {
-				gameData.playerX = newX;
+			if (!isPositionOccupied(newX, newY) && !isLeaveEdge(newX, newY)) {
+				gameData.window.playerX = newX;
 				gameData.playerWorldX += 1;
 			}
 			break;
 		}
 		case ("s"):
 		case("ArrowDown"): { // 下箭头键
-			var newX = gameData.playerX;
-			var newY = gameData.playerY + 1;
+			var newX = gameData.window.playerX;
+			var newY = gameData.window.playerY + 1;
 
-			if (!isPositionOccupied(newX, newY) && !isLeaveedge(newX, newY)) {
-				gameData.playerY = newY;
+			if (!isPositionOccupied(newX, newY) && !isLeaveEdge(newX, newY)) {
+				gameData.window.playerY = newY;
 				gameData.playerWorldY += 1;
 			}
 			break;
@@ -161,8 +192,8 @@ document.addEventListener('keyup', function(event) {
 });
 
 // 判断是否南开边缘
-function isLeaveedge(x = 0, y = 0) {
-	if (x >= gameData.width || x < 0 || y >= gameData.height || y < 0) {
+function isLeaveEdge(x = 0, y = 0) {
+	if (x >= gameData.window.width || x < 0 || y >= gameData.window.height || y < 0) {
 		return true;
 	}
 	else {
@@ -172,10 +203,10 @@ function isLeaveedge(x = 0, y = 0) {
 
 // 判断位置是否被占用
 function isPositionOccupied(x, y) {
-	if (x === gameData.playerX && y === gameData.playerY) {
+	if (x === gameData.window.playerX && y === gameData.window.playerY) {
 		return true;
 	}
-	if (isLeaveedge(x, y)) {
+	if (isLeaveEdge(x, y)) {
 		return false;
 	}
 	for (var i = 0; i < gameData.enemies.length; i++) {
